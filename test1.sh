ff795a3c17950ba24d55d32da42f5f2ea8f1eda5
kattasandeep6191 <kattasandeep6191@gmail.com>
2024-08-17 14:39:51 +0530

    added folder

---
+++ test1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x -e
+numone=100
+numtwo=200
+echo ${numone}
+echo ${numtwo}
+useradd -m testuser1
+useradd -m testuser2
+useradd -m testuser1
+useradd -m testuser4
+useradd -m testuser5
